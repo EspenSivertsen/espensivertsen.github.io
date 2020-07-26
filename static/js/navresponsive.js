b16df084491cb298f6dd923ac7cbb666c33ea0eb
Espen Sivertsen <EspenSivertsen@users.noreply.github.com>
2020-07-26 15:00:53 +0200

    Change from 1 page to multiple
    Changed design completely.
    Added transitions, smooth-scroll, sticky navbar, links, project-boxes to display projects better, added contact form.
    Added images, and created css arrows.

--- static/js/navresponsive.js
+++ static/js/navresponsive.js
@@ -1,7 +1,7 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsenav() {
     var x = document.getElementById("navbar");
-    if (x.className === "nav") {
+    if (x.className === "nav" || x.className === "nav sticky") {
         x.className += " responsive";
     } else {
         x.className = "nav";
